41287ba29138367bd9a0a5655514f4df6d0700cf
sarveshsataye36 <sarveshsataye36@gmail.com>
2023-05-23 16:04:35 +0530

    commit

---
+++ controllers/service.controller.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+* **************************************************************************************
+* @tutorial: Service Controller
+* **************************************************************************************
+*
+* Created on 23-May-2023 2:27:00 pm
+*
+* @FileName: Service.controller.php
+* @Author: Stackbuz Technologies Team
+* @AuthorEmailId:  info@stackbuz.com
+* @ProjectName:
+* @version 1.0.0
+* @Copyright ( c ) 2022 www.stackbuz.com
+* @All rights reserved
+*  Copyright 2022 Stackbuz Technologies
+*
+* @ModificaionHistory
+*  MODIFICATION DATE:
+*  AUTHOR:
+*  REASON:
+*
+*/
+?>
+<?php
+
+class ServiceController extends Controller {
+    //
+
+    public function __construct( $parameter ) {
+    }
+    //
+
+    public function index() {
+    }
+}
+
+?>
